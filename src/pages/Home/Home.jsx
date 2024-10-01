999f281de2df6602b3ca861e2cff3992e4b0610b
Aurore  <hoshizora972@gmail.com>
2024-10-01 09:28:17 -0400

    end configuration

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import NavBar from '../../components/Navbar/NavBar'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default Home
